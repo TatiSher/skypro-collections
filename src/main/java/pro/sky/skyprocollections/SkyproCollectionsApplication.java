f4637f383024e8b0af5d69eb8328602444e6343d
Tatiana <tanja6122212@list.ru>
2021-12-30 00:30:47 +0900

    Initial commit

---
+++ src/main/java/pro/sky/skyprocollections/SkyproCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCollectionsApplication.class, args);
+	}
+
+}
